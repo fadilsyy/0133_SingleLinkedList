efc5ebaa2e1c2a181233d917d02891f804c482e6
fadilsyy <fadilasytar@gmail.com>
2025-05-08 15:40:59 +0700

    linkedlist

--- linkedList.cpp
+++ linkedList.cpp
@@ -72,6 +72,28 @@ public:
             *previous = *current;
             *current = (*current)->next;
         }
+
+        return (*current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+        
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current ->next;
+        
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
         
     }
+    
 };
