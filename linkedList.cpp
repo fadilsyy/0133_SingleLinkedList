44c9148f05d7513f846921d8f3a7891c80ffcac2
fadilsyy <fadilasytar@gmail.com>
2025-05-08 15:14:29 +0700

    linkedlist

--- linkedList.cpp
+++ linkedList.cpp
@@ -8,3 +8,30 @@ public:
     int noMhs;
     Node *next;
 };
+
+class List
+{
+    Node *START;
+
+public:
+    List()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "/nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+        
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            
+        }
+        
+    }
+};
